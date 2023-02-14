8d6f1b4a773baa6dbe74407c0d7b0ca1b3d32e8d
Josh Helzerman <joshhelzerman@gmail.com>
2023-02-14 09:19:48 -0800

    ads

--- GizmoCommander/gizmoCommander.py
+++ GizmoCommander/gizmoCommander.py
@@ -120,7 +120,7 @@ def parseArgs():
     args = argParser.parse_args()
     EEG_PORT = args.eeg_port
     GIZMO_PORT = args.gizmo_port
-    GIZMO_DIRECTOR_PORT = args.gizmo_dorector_port
+    GIZMO_DIRECTOR_PORT = args.gizmo_director_port
     GIZMO_IP = args.gizmo_director_address
 
 
